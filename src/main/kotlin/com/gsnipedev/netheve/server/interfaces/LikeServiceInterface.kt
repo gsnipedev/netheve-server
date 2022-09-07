51c77ca8e031fb04a70cda5663ada6abac38a3a5
dika <andikawpb200@gmail.com>
2022-09-07 17:35:38 +0700

    Add Like/Unlike/Dislike feature

---
+++ src/main/kotlin/com/gsnipedev/netheve/server/interfaces/LikeServiceInterface.kt
@@ -0,0 +1,19 @@
+package com.gsnipedev.netheve.server.interfaces
+
+import com.gsnipedev.netheve.server.entity.LikeEntity
+import com.gsnipedev.netheve.server.model.WebResponse
+import com.gsnipedev.netheve.server.model.like.LikeModel
+
+interface LikeServiceInterface {
+
+    fun like(data: LikeModel): WebResponse<String>
+
+    fun unlike(data: Int): WebResponse<String>
+
+    fun dislike(data: LikeModel): WebResponse<String>
+
+    fun getAllLikesById(postId: Int): WebResponse<List<LikeEntity>>
+
+    fun getTotalLikesById(postId: Int): WebResponse<Int>
+
+}
